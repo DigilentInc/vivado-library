40c4739f609d267b3bd4035153538ea6b62fbaf9
tom21091 <tom.kappenman@wsu.edu>
2016-04-11 18:47:57 -0700

    ip: Pmods: pmodOLEDrgb: edited main.c example file

--- ip/Pmods/pmodOLEDrgb_v1_0/drivers/PmodOLEDrgb_v1_0/examples/main.c
+++ ip/Pmods/pmodOLEDrgb_v1_0/drivers/PmodOLEDrgb_v1_0/examples/main.c
@@ -18,7 +18,7 @@ PmodOLEDrgb oled;
 int main(void)
 {
 	Xil_ICacheEnable();
-	//Xil_DCacheEnable();
+	Xil_DCacheEnable();
 	DemoInitialize();
 	DemoRun();
 	return 0;
@@ -26,7 +26,7 @@ int main(void)
 
 void DemoInitialize()
 {
-	Oledrgb_begin(&oled, XPAR_PMODOLEDRGB_0_AXI_LITE_GPIO_BASEADDR, XPAR_PMODOLEDRGB_0_AXI_LITE_SPI_BASEADDR);
+	OLEDrgb_begin(&oled, XPAR_PMODOLEDRGB_0_AXI_LITE_GPIO_BASEADDR, XPAR_PMODOLEDRGB_0_AXI_LITE_SPI_BASEADDR);
 }
 
 
@@ -35,17 +35,17 @@ void DemoRun()
 	int i=0;
 	while(1){
 		if (i==1){
-			DrawBitmap(&oled, 0,0,95,63, (uint8_t*)tommy);
+			OLEDrgb_DrawBitmap(&oled, 0,0,95,63, (uint8_t*)tommy);
 			break;
 		}
 		else {
-			DrawBitmap(&oled, 0, 0, 95,63, (uint8_t*)snake1);
-			DrawBitmap(&oled, 0, 0, 95,63, (uint8_t*)snake2);
-			DrawBitmap(&oled, 0, 0, 95,63, (uint8_t*)snake3);
-			DrawBitmap(&oled, 0, 0, 95,63, (uint8_t*)snake2);
+			OLEDrgb_DrawBitmap(&oled, 0, 0, 95,63, (uint8_t*)snake1);
+			OLEDrgb_DrawBitmap(&oled, 0, 0, 95,63, (uint8_t*)snake2);
+			OLEDrgb_DrawBitmap(&oled, 0, 0, 95,63, (uint8_t*)snake3);
+			OLEDrgb_DrawBitmap(&oled, 0, 0, 95,63, (uint8_t*)snake2);
 		}
 		/*for (i =0; i<96; i++){
-			DrawRectangle(&oled, 0,0,i,63,BuildRGB(i,i*3,i*4),true,BuildRGB(i,i*3,i*4));
+			OLEDrgb_DrawRectangle(&oled, 0,0,i,63,BuildRGB(i,i*3,i*4),true,BuildRGB(i,i*3,i*4));
 		}*/
 	}
 }

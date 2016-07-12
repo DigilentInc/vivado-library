bc53aaa8c5f746108e3799e3f606601b9ef43977
Arthur Brown <arthur.brown@email.wsu.edu>
2016-07-12 13:29:34 -0700

    ip: Pmods: PmodOLED: edited comments

--- ip/Pmods/PmodOLED_v1_0/drivers/PmodOLED_v1_0/examples/main.c
+++ ip/Pmods/PmodOLED_v1_0/drivers/PmodOLED_v1_0/examples/main.c
@@ -1,49 +1,42 @@
-/******************************************************************************
-*
-* Copyright (C) 2009 - 2014 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* Use of the Software is limited solely to applications:
-* (a) running on a Xilinx device, or
-* (b) that interact with a Xilinx device through a bus or interconnect.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
-* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*
-* Except as contained in this notice, the name of the Xilinx shall not be used
-* in advertising or otherwise to promote the sale, use or other dealings in
-* this Software without prior written authorization from Xilinx.
-*
-******************************************************************************/
 
+/************************************************************************/
+/*																		*/
+/* input_demo.c		demo project for the PmodGPIO IP 					*/
+/*																		*/
+/************************************************************************/
+/*	Author:		Arthur Brown											*/
+/*	Copyright 2016, Digilent Inc.										*/
+/************************************************************************/
 /*
- *
- *
- * This application configures UART 16550 to baud rate 9600.
- * PS7 UART (Zynq) is not initialized by this application, since
- * bootrom/bsp configures it to baud rate 115200
- *
- * ------------------------------------------------
- * | UART TYPE   BAUD RATE                        |
- * ------------------------------------------------
- *   uartns550   9600
- *   uartlite    Configurable only in HW design
- *   ps7_uart    115200 (configured by bootrom/bsp)
- */
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/************************************************************************/
+/*  File Description:													*/
+/*																		*/
+/*	This demo_project initializes and uses the PmodOLED to display strings and show different available fill patterns.							*/
+/*																		*/
+/************************************************************************/
+/*  Revision History:													*/
+/*																		*/
+/*	06/20/2016(ArtVVB): Created 											*/
+/*																		*/
+/************************************************************************/
+/*  Baud Rates:															*/
+/*																		*/
+/*	Microblaze: 9600 or what was specified in UARTlite core				*/
+/*	Zynq: 115200														*/
+/*																		*/
+/************************************************************************/
 
 /* ------------------------------------------------------------ */
 /*				Include File Definitions						*/
@@ -55,7 +48,6 @@
 #include "xil_printf.h"
 
 #include "PmodOLED.h"
-//#include "OLED.h"
 
 /* ------------------------------------------------------------ */
 /*				Global Variables								*/

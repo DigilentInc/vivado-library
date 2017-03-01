ccea8fe2825f6a034adfa43f7ba32b84c89e8d59
Arthur Brown <arthur.brown@email.wsu.edu>
2017-03-01 14:16:34 -0800

    ip: Pmods: PmodTC1: Validation for 2015.4
    removed unnecessary index.html
    renamed temperature conversion function
    cleaned header includes
    updated comment headers

--- ip/Pmods/PmodTC1_v1_0/drivers/PmodTC1_v1_0/src/PmodTC1.h
+++ ip/Pmods/PmodTC1_v1_0/drivers/PmodTC1_v1_0/src/PmodTC1.h
@@ -1,36 +1,30 @@
+/************************************************************************/
+/*																		*/
+/*	PmodR2R.h		--		Declaration for Pmod TC1 library		 	*/
+/*																		*/
+/************************************************************************/
+/*	Author:		Arthur Brown											*/
+/*	Copyright 2017, Digilent Inc.										*/
+/************************************************************************/
+/*  File Description:													*/
+/*	This file declares the TC1 library functions.						*/
+/*																		*/
+/************************************************************************/
+/*  Revision History:													*/
+/*																		*/
+/*	03/01/2017(ArtVVB): Validated for Vivado 2015.4						*/
+/*																		*/
+/************************************************************************/
 
 #ifndef PMODTC1_H
 #define PMODTC1_H
 
-
 /****************** Include Files ********************/
+
 #include "xil_types.h"
 #include "xstatus.h"
 #include "xspi_l.h"
 #include "xspi.h"
-
-/* ------------------------------------------------------------ */
-/*					Definitions									*/
-/* ------------------------------------------------------------ */
-#define bool u8
-#define true 1
-#define false 0
-
- 
-/* ------------------------------------------------------------ */
-/*		Register addresses Definitions							*/
-/* ------------------------------------------------------------ */
-
-/* ------------------------------------------------------------ */
-/*				Bit masks Definitions							*/
-/* ------------------------------------------------------------ */
-
-
-/* ------------------------------------------------------------ */
-/*				Parameters Definitions							*/
-/* ------------------------------------------------------------ */
-
-
 
 /* ------------------------------------------------------------ */
 /*					Procedure Declarations						*/
@@ -47,11 +41,6 @@ int TC1_SPIInit(XSpi *SpiInstancePtr);
 void TC1_readData(PmodTC1* InstancePtr);
 double TC1_getTemp(PmodTC1* InstancePtr);
 double TC1_getAMBTemp(PmodTC1* InstancePtr);
-double TC1_celToFar(double celsius);
-
-
-
-
-
+double TC1_tempC2F(double celsius);
 
 #endif // PMODTC1_H

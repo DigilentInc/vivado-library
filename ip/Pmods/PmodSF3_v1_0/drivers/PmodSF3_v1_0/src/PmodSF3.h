a0de21c78668a4d1bf7e91a3c9f87bca9ebff03e
Arthur Brown <arthur.brown@email.wsu.edu>
2017-07-26 14:38:14 -0700

    Refactored and Validated Drivers and Demo
    Moved Interrupt controller initialization to demo-side.
    Moved test function to demo-side.
    Moved buffers to demo-side.
    Removed call to SPI self-test.
    Added SF3 identifier to all driver macros.

--- ip/Pmods/PmodSF3_v1_0/drivers/PmodSF3_v1_0/src/PmodSF3.h
+++ ip/Pmods/PmodSF3_v1_0/drivers/PmodSF3_v1_0/src/PmodSF3.h
@@ -1,3 +1,23 @@
+/*************************************************************************/
+/*                                                                       */
+/*     PmodSF3.h --     PmodSF3 Driver Header                         	 */
+/*                                                                       */
+/*************************************************************************/
+/*     Author: Andrew Holzer                                             */
+/*     Copyright 2017, Digilent Inc.                                     */
+/*************************************************************************/
+/*  Module Description:                                                  */
+/*                                                                       */
+/*            This file contains driver prototypes, types, and macros    */
+/*            for the Pmod SF3 driver source.							 */
+/*                                                                       */
+/*************************************************************************/
+/*  Revision History:                                                    */
+/*                                                                       */
+/*            ??/??/????(AHolzer): created                               */
+/*            07/26/2017(ArtVVB): Refactored and Validated               */
+/*                                                                       */
+/*************************************************************************/
 
 #ifndef PMODSF3_H_
 #define PMODSF3_H_
@@ -18,50 +38,49 @@ extern "C" {
 #include "xstatus.h"
 #include "xparameters.h"
 
+#define SF3_MANUFACTURER_ID 0x20
 
-#ifdef XPAR_INTC_0_DEVICE_ID
-#define SPI_INTR_ID 		XPAR_INTC_0_SPI_0_VEC_ID
-#else
-#define SPI_INTR_ID			XPAR_FABRIC_SPI_0_VEC_ID
-#endif
-
-
-
-#define MANUFACTURER_ID 0x20
-
-#define COMMAND_PAGE_PROGRAM 	0x02 // Page program command
-#define COMMAND_QUAD_WRITE 		0x32 // Quad Input Fast Program
-#define COMMAND_RANDOM_READ 	0x03 // Random read command
-#define COMMAND_DUAL_READ		0x3B // Dual Output Fast Read
-#define COMMAND_DUAL_IO_READ	0xBB // Dual IO Fast Read
-#define COMMAND_QUAD_READ		0x6B // Quad Output Fast Read
-#define COMMAND_QUAD_IO_READ	0xEB // Quad IO Fast Read
-#define COMMAND_WRITE_ENABLE 	0x06 // Write Enable command
-#define COMMAND_SECTOR_ERASE	0xD8 // Sector Erase command
-#define COMMAND_BULK_ERASE		0xC7 // Bulk Erase command
-#define COMMAND_READ_STATUSREG 	0x05 // Status read command
-#define COMMAND_READ_ID 		0x9E // ID read command
-#define COMMAND_WRITE_STATUSREG 0x01 // Status write command
+#define SF3_COMMAND_PAGE_PROGRAM 	0x02 // Page program command
+#define SF3_COMMAND_QUAD_WRITE 		0x32 // Quad Input Fast Program
+#define SF3_COMMAND_RANDOM_READ 	0x03 // Random read command
+#define SF3_COMMAND_DUAL_READ		0x3B // Dual Output Fast Read
+#define SF3_COMMAND_DUAL_IO_READ	0xBB // Dual IO Fast Read
+#define SF3_COMMAND_QUAD_READ		0x6B // Quad Output Fast Read
+#define SF3_COMMAND_QUAD_IO_READ	0xEB // Quad IO Fast Read
+#define SF3_COMMAND_WRITE_ENABLE 	0x06 // Write Enable command
+#define SF3_COMMAND_SECTOR_ERASE	0xD8 // Sector Erase command
+#define SF3_COMMAND_BULK_ERASE		0xC7 // Bulk Erase command
+#define SF3_COMMAND_READ_STATUSREG 	0x05 // Status read command
+#define SF3_COMMAND_READ_ID 		0x9E // ID read command
+#define SF3_COMMAND_WRITE_STATUSREG 0x01 // Status write command
 
 /*
  * These definitions specify the EXTRA bytes in each of the command transactions.
  * This count includes the Command byte, address bytes and any don't care bytes needed.
  */
-#define READ_WRITE_EXTRA_BYTES 	4
-#define WRITE_ENABLE_BYTES 		1
-#define SECTOR_ERASE_BYTES		4
-#define BULK_ERASE_BYTES		1
-#define STATUS_READ_BYTES		2
-#define STATUS_WRITE_BYTES		2
+#define SF3_WRITE_EXTRA_BYTES 		4
+#define SF3_READ_MIN_EXTRA_BYTES 	4
+#define SF3_READ_MAX_EXTRA_BYTES 	9
+#define SF3_WRITE_ENABLE_BYTES 		1
+#define SF3_SECTOR_ERASE_BYTES		4
+#define SF3_BULK_ERASE_BYTES		1
+#define SF3_STATUS_READ_BYTES		2
+#define SF3_STATUS_WRITE_BYTES		2
+#define SF3_ID_READ_BYTES			20
 
 // Flash not busy mask in the status register of the flash device
-#define FLASH_SR_IS_READY_MASK 0x01
+#define SF3_FLASH_SR_IS_READY_MASK 0x01
 
 // Number of bytes per page in the flash device
-#define PAGE_SIZE 256
+#define SF3_PAGE_SIZE 				256
+
+#define SF3_PAGES_PER_SUBSECTOR		16
+#define SF3_SUBSECTORS_PER_SECTOR	16
+#define SF3_NUM_SECTORS				8192
+#define SF3_MAX_ADDRESS 			0x1FFFFFF
 
 // Address of the page to perform erase, write and read operations.
-#define FLASH_TEST_ADDRESS 0xC0000
+#define SF3_FLASH_TEST_ADDRESS	0x004000
 
 /*
  * The following definitions specify the number of dummy bytes to ignore in the
@@ -75,36 +94,31 @@ extern "C" {
  * same logic applies to Quad read command, so we end up with 4 dummy bytes in
  * that case
  */
-#define DUAL_READ_DUMMY_BYTES 2
-#define QUAD_READ_DUMMY_BYTES 4
+#define SF3_DUAL_READ_DUMMY_BYTES 2
+#define SF3_QUAD_READ_DUMMY_BYTES 4
 
-#define DUAL_IO_READ_DUMMY_BYTES 2
-#define QUAD_IO_READ_DUMMY_BYTES 5
+#define SF3_DUAL_IO_READ_DUMMY_BYTES 2
+#define SF3_QUAD_IO_READ_DUMMY_BYTES 5
 
 typedef struct PmodSF3 {
 	XSpi SF3Spi;
 	INTC sIntc;
-	u8 ReadBuffer[PAGE_SIZE + READ_WRITE_EXTRA_BYTES + 4];
-	u8 WriteBuffer[PAGE_SIZE + READ_WRITE_EXTRA_BYTES];
-}PmodSF3;
+	u8 Status;
+} PmodSF3;
 
 /************************** Function Prototypes ******************************/
-XStatus SF3_begin(PmodSF3* InstancePtr, const ivt_t ivt[], u32 SPI_Address);
+XStatus SF3_begin(PmodSF3* InstancePtr, INTC* IntcPtr, const ivt_t ivt[], u32 SPI_Address);
 XStatus SF3_SpiInit(XSpi *SpiPtr);
-XStatus SF3_FlashWriteEnable(PmodSF3 *SpiPtr);
-XStatus SF3_FlashWrite(PmodSF3 *SpiPtr, u32 Addr, u32 ByteCount, u8 WriteCmd);
-XStatus SF3_FlashRead(PmodSF3 *SpiPtr, u32 Addr, u32 ByteCount, u8 ReadCmd);
-XStatus SF3_BulkErase(PmodSF3* SpiPtr);
-XStatus SF3_SectorErase(PmodSF3 *SpiPtr, u32 Addr);
-XStatus SF3_GetStatus(PmodSF3 *SpiPtr);
-XStatus SF3_WaitForFlashReady(PmodSF3 *SpiPtr);
-XStatus SF3_QuadSpiTest(PmodSF3 *SpiPtr);
-XStatus SF3_ReadID(PmodSF3 *SpiPtr);
+XStatus SF3_FlashWriteEnable(PmodSF3 *InstancePtr);
+XStatus SF3_FlashWrite(PmodSF3 *InstancePtr, u32 Addr, u32 ByteCount, u8 WriteCmd, u8 **BufferPtr);
+XStatus SF3_FlashRead(PmodSF3 *InstancePtr, u32 Addr, u32 ByteCount, u8 ReadCmd, u8 **BufferPtr);
+XStatus SF3_BulkErase(PmodSF3* InstancePtr);
+XStatus SF3_SectorErase(PmodSF3 *InstancePtr, u32 Addr);
+XStatus SF3_GetStatus(PmodSF3 *InstancePtr);
+XStatus SF3_WaitForFlashReady(PmodSF3 *InstancePtr);
+XStatus SF3_ReadID(PmodSF3 *InstancePtr);
 
-void SpiHandler(void *CallBackRef, u32 StatusEvent, unsigned int ByteCount);
-
-XStatus updateErrorCount();
-void SF3_printID(u8 *idData);
+void SF3_SpiHandler(void *CallBackRef, u32 StatusEvent, unsigned int ByteCount);
 
 #ifdef __cplusplus
 }

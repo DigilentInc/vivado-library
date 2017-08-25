e3cf345daa3f4abbbd328f6c30245c64f995d50f
Arthur Brown <arthur.brown@email.wsu.edu>
2017-08-25 11:13:24 -0700

    ip: Pmods: PmodALS: 2015.4 Maintenance
    Formatted drivers and main
    Added comment headers

--- ip/Pmods/PmodALS_v1_0/drivers/PmodALS_v1_0/src/PmodALS.c
+++ ip/Pmods/PmodALS_v1_0/drivers/PmodALS_v1_0/src/PmodALS.c
@@ -1,132 +1,129 @@
-
+/************************************************************************/
+/*                                                                      */
+/* PmodALS.c    --      Driver source for the PmodALS                   */
+/*                                                                      */
+/************************************************************************/
+/*  File Description:                                                   */
+/*                                                                      */
+/*  This file contains the drivers for the PmodALS IP from Digilent     */
+/*                                                                      */
+/************************************************************************/
+/*  Revision History:                                                   */
+/*                                                                      */
+/*  03/31/2016(tkappenman): Created                                     */
+/*  08/24/2017(artvvb): Validated for Vivado 2015.4                     */
+/*                                                                      */
+/************************************************************************/
 
 /***************************** Include Files *******************************/
+
 #include "PmodALS.h"
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 /************************** Function Definitions ***************************/
+
 XSpi_Config XSpi_ALSConfig =
 {
-	0,
-	0,
-	1,
-	0,
-	1,
-	8,
-	0,
-	0,
-	0,
-	0,
-	0
+    0,
+    0,
+    1,
+    0,
+    1,
+    8,
+    0,
+    0,
+    0,
+    0,
+    0
 };
+
 /* ------------------------------------------------------------ */
-/***	void ALS_begin(PmodALS* InstancePtr, u32 GPIO_Address, u32 SPI_Address)
+/***    XStatus ALS_begin(PmodALS* InstancePtr, u32 GPIO_Address, u32 SPI_Address)
 **
-**	Parameters:
-**		InstancePtr: A PmodALS object to start
-**		GPIO_Address: The Base address of the PmodALS GPIO
-**		SPI_Address: The Base address of the PmodALS SPI
+**  Parameters:
+**      InstancePtr: A PmodALS device to start
+**      SPI_Address: The base address of the PmodALS SPI device
 **
-**	Return Value:
-**		none
+**  Return Value:
+**      none
 **
-**	Errors:
-**		none
-**
-**	Description:
-**		Initialize the PmodALS.
+**  Description:
+**      Initialize the PmodALS.
 */
-void ALS_begin(PmodALS* InstancePtr, u32 SPI_Address)
+XStatus ALS_begin(PmodALS* InstancePtr, u32 SPI_Address)
 {
-	XSpi_ALSConfig.BaseAddress=SPI_Address;
-	ALS_SPIInit(&InstancePtr->ALSSpi);
+    XSpi_ALSConfig.BaseAddress=SPI_Address;
+    return ALS_SPIInit(&InstancePtr->ALSSpi);
 }
 
 /* ------------------------------------------------------------ */
-/***	ALS_SPIInit
+/***    XStatus ALS_SPIInit(XSpi *SpiInstancePtr)
 **
-**	Parameters:
-**		none
+**  Parameters:
+**      SpiInstancePtr: ALS AXI SPI device to initialize
 **
-**	Return Value:
-**		none
+**  Return Value:
+**      failure or success status
 **
-**	Errors:
-**		none
-**
-**	Description:
-**		Initializes the PmodALS SPI.
-
+**  Description:
+**      Initializes the PmodALS SPI.
 */
+XStatus ALS_SPIInit(XSpi *SpiInstancePtr)
+{
+    XStatus Status;
+    XSpi_Config *ConfigPtr;
 
-int ALS_SPIInit(XSpi *SpiInstancePtr){
-	int Status;
-	XSpi_Config *ConfigPtr; /* Pointer to Configuration data */
+    /*
+     * Initialize the SPI driver so that it is ready to use.
+     */
+    ConfigPtr = &XSpi_ALSConfig;
+    if (ConfigPtr == NULL) {
+        return XST_DEVICE_NOT_FOUND;
+    }
 
-	/*
-	 * Initialize the SPI driver so that it is  ready to use.
-	 */
-	ConfigPtr = &XSpi_ALSConfig;
-	if (ConfigPtr == NULL) {
-		return XST_DEVICE_NOT_FOUND;
-	}
+    Status = XSpi_CfgInitialize(SpiInstancePtr, ConfigPtr, ConfigPtr->BaseAddress);
+    if (Status != XST_SUCCESS) {
+        return XST_FAILURE;
+    }
 
-	Status = XSpi_CfgInitialize(SpiInstancePtr, ConfigPtr, ConfigPtr->BaseAddress);
-	if (Status != XST_SUCCESS) {
-		return XST_FAILURE;
-	}
+    Status = XSpi_SetOptions(SpiInstancePtr, (XSP_MASTER_OPTION | XSP_CLK_ACTIVE_LOW_OPTION | XSP_CLK_PHASE_1_OPTION) | XSP_MANUAL_SSELECT_OPTION); //Manual SS off
+    if (Status != XST_SUCCESS) {
+        return XST_FAILURE;
+    }
 
+    Status = XSpi_SetSlaveSelect(SpiInstancePtr, 1);
+    if (Status != XST_SUCCESS) {
+        return XST_FAILURE;
+    }
 
+    /*
+     * Start the SPI driver so that the device is enabled.
+     */
+    XSpi_Start(SpiInstancePtr);
 
-	Status = XSpi_SetOptions(SpiInstancePtr, (XSP_MASTER_OPTION | XSP_CLK_ACTIVE_LOW_OPTION | XSP_CLK_PHASE_1_OPTION) | XSP_MANUAL_SSELECT_OPTION); //Manual SS off
-	if (Status != XST_SUCCESS) {
-		return XST_FAILURE;
-	}
+    /*
+     * Disable Global interrupt to use polled mode operation
+     */
+    XSpi_IntrGlobalDisable(SpiInstancePtr);
 
-	Status = XSpi_SetSlaveSelect(SpiInstancePtr, 1);
-	if (Status != XST_SUCCESS) {
-		return XST_FAILURE;
-	}
-
-	/*
-	 * Start the SPI driver so that the device is enabled.
-	 */
-	XSpi_Start(SpiInstancePtr);
-
-	/*
-	 * Disable Global interrupt to use polled mode operation
-	 */
-	XSpi_IntrGlobalDisable(SpiInstancePtr);
-
-	return XST_SUCCESS;
-
+    return XST_SUCCESS;
 }
 
 /* ------------------------------------------------------------ */
-/***	ALS_Read
+/***    u8 ALS_read(PmodALS* InstancePtr)
 **
-**	Synopsis:
-**		light = ALS_Read(&ALS)
+**  Parameters:
+**      PmodALS *InstancePtr    - the PmodALS device to communicate with
 **
-**	Parameters:
-**		PmodALS *InstancePtr	- the PmodALS object to communicate with
+**  Return Value:
+**      u8 light    - Light level from PmodALS
 **
-**	Return Value:
-**		u8 light	- Light level from PmodALS
-**
-**	Errors:
-**		none
-**
-**	Description:
-**		Reads ambient light sensor
+**  Description:
+**      Captures ambient light sensor data
 */
-
-u8 ALS_read(PmodALS* InstancePtr){
-	u8 light[2];
-	XSpi_Transfer(&InstancePtr->ALSSpi, light, light, 2);
-	return light[0]<<3|light[1]>>5;
+u8 ALS_read(PmodALS* InstancePtr)
+{
+    u8 light[2];
+    XSpi_Transfer(&InstancePtr->ALSSpi, light, light, 2);
+    return (light[0] << 3) | (light[1] >> 5);
 }
-
 
--- ip/Pmods/PmodALS_v1_0/drivers/PmodALS_v1_0/src/PmodALS.h
+++ ip/Pmods/PmodALS_v1_0/drivers/PmodALS_v1_0/src/PmodALS.h
@@ -1,51 +1,44 @@
+/************************************************************************/
+/*                                                                      */
+/* PmodALS.h    --      Driver definitions for the PmodALS              */
+/*                                                                      */
+/************************************************************************/
+/*  File Description:                                                   */
+/*                                                                      */
+/*  This file contains the drivers for the PmodALS IP from Digilent     */
+/*                                                                      */
+/************************************************************************/
+/*  Revision History:                                                   */
+/*                                                                      */
+/*  03/31/2016(tkappenman): Created                                     */
+/*  08/24/2017(artvvb): Validated for Vivado 2015.4                     */
+/*                                                                      */
+/************************************************************************/
 
 #ifndef PMODALS_H
 #define PMODALS_H
 
-
 /****************** Include Files ********************/
+
 #include "xil_types.h"
 #include "xstatus.h"
 #include "xspi_l.h"
 #include "xspi.h"
 
 /* ------------------------------------------------------------ */
-/*					Definitions									*/
-/* ------------------------------------------------------------ */
-#define bool u8
-#define true 1
-#define false 0
-
-
-/* ------------------------------------------------------------ */
-/*		Register addresses Definitions							*/
+/*                  Definitions                                 */
 /* ------------------------------------------------------------ */
 
-/* ------------------------------------------------------------ */
-/*				Bit masks Definitions							*/
-/* ------------------------------------------------------------ */
-
-
-/* ------------------------------------------------------------ */
-/*				Parameters Definitions							*/
-/* ------------------------------------------------------------ */
-
-
+typedef struct PmodALS {
+    XSpi ALSSpi;
+} PmodALS;
 
 /* ------------------------------------------------------------ */
-/*					Procedure Declarations						*/
+/*                  Procedure Declarations                      */
 /* ------------------------------------------------------------ */
 
-typedef struct PmodALS{
-	XSpi ALSSpi;
-}PmodALS;
-
-void ALS_begin(PmodALS* InstancePtr, u32 SPI_Address);
-int ALS_SPIInit(XSpi *SpiInstancePtr);
+XStatus ALS_begin(PmodALS* InstancePtr, u32 SPI_Address);
+XStatus ALS_SPIInit(XSpi *SpiInstancePtr);
 u8 ALS_read(PmodALS* InstancePtr);
-
-
-
-
 
 #endif // PMODALS_H

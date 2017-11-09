f62b52b1e2fbc0d428d7bc60aca32229fe25b65f
Arvin Tang <atangzwj@outlook.com>
2017-11-08 16:01:21 -0800

    ip: Pmods: PmodHYGRO: Validated for 2016.4 Upgraded IPs Formatted driver and example code Added README Added .gitignore

--- ip/Pmods/PmodHYGRO_v1_0/drivers/PmodHYGRO_v1_0/examples/main.c
+++ ip/Pmods/PmodHYGRO_v1_0/drivers/PmodHYGRO_v1_0/examples/main.c
@@ -1,42 +1,45 @@
-/***************************************************************************/
-/*																		   */
-/*	main.c		--		Definition for HYGROI2C demo	 	    		   */
-/*																		   */
-/***************************************************************************/
-/*	Author:		Arthur Brown											   */
-/*	Copyright 2017, Digilent Inc.										   */
-/***************************************************************************/
-/*  File Description:													   */
-/*	This file defines a demonstration for the use of the Pmod HYGRO ip     */
-/*  core. Temperature and Humidity data is captured from the I2C  		   */
-/*  connection and displayed over the serial connection once per second.   */
-/*  This application configures UART 16550 to baud rate 9600.			   */
-/*  PS7 UART (Zynq) is not initialized by this application, since		   */
-/*  bootrom/bsp configures it to baud rate 115200						   */
-/*																		   */
-/*   UART TYPE   BAUD RATE                        						   */
-/*   uartns550   9600													   */
-/*   uartlite    Configurable only in HW design							   */
-/*   ps7_uart    115200 (configured by bootrom/bsp)						   */
-/*																		   */
-/***************************************************************************/
-/*  Revision History:													   */
-/*	01/30/2017(ArtVVB): created											   */
-/*	02/21/2017(ArtVVB): validated										   */
-/***************************************************************************/
+/******************************************************************************/
+/*                                                                            */
+/* main.c -- Definition for HYGROI2C demo                                     */
+/*                                                                            */
+/******************************************************************************/
+/* Author: Arthur Brown                                                       */
+/* Copyright 2017, Digilent Inc.                                              */
+/******************************************************************************/
+/* File Description:                                                          */
+/*                                                                            */
+/* This file defines a demonstration for the use of the Pmod HYGRO IP core.   */
+/* Temperature and humidity data are captured from the I2C connection and     */
+/* displayed over the serial connection once per second.                      */
+/*                                                                            */
+/* This application configures UART 16550 to Baud rate 9600. PS7 UART (Zynq)  */
+/* is not initialized by this application, since bootrom/bsp configures it to */
+/* Baud rate 115200.                                                          */
+/*                                                                            */
+/*    UART TYPE   BAUD RATE                                                   */
+/*    uartns550   9600                                                        */
+/*    uartlite    Configurable only in HW design                              */
+/*    ps7_uart    115200 (configured by bootrom/bsp)                          */
+/*                                                                            */
+/******************************************************************************/
+/* Revision History:                                                          */
+/*                                                                            */
+/*    01/30/2017(ArtVVB):   created                                           */
+/*    02/21/2017(ArtVVB):   validated                                         */
+/*    11/08/2017(atangzwj): Validated for Vivado 2016.4                       */
+/*                                                                            */
+/******************************************************************************/
 
+#include "PmodHYGRO.h"
+#include "sleep.h"
+#include "xil_cache.h"
 #include "xil_printf.h"
 #include "xparameters.h"
-#include "PmodHYGRO.h"
-#include "xparameters.h"
-#include "xil_cache.h"
+
 #ifdef __MICROBLAZE__
-	#include "microblaze_sleep.h"
-	//(deprecated in Vivado 2016.4)
-	#define TIMER_FREQ_HZ XPAR_CPU_M_AXI_DP_FREQ_HZ
+#define TIMER_FREQ_HZ XPAR_CPU_M_AXI_DP_FREQ_HZ
 #else
-	#include "sleep.h"
-	#define TIMER_FREQ_HZ 100000000
+#define TIMER_FREQ_HZ 100000000
 #endif
 
 PmodHYGRO myDevice;
@@ -48,79 +51,75 @@ void EnableCaches();
 void DisableCaches();
 void DemoSleep(int millis);
 
-int main()
-{
-	DemoInitialize();
-	DemoRun();
-	DemoCleanup();
+int main() {
+   DemoInitialize();
+   DemoRun();
+   DemoCleanup();
 
-    return 0;
+   return 0;
 }
 
-void DemoInitialize()
-{
-	EnableCaches();
-    xil_printf("Init Started\n\r");
-    HYGRO_begin (
-    		&myDevice,
-    		XPAR_PMODHYGRO_0_AXI_LITE_IIC_BASEADDR,
-    		0x40, // chip address of PmodHYGRO IIC
-    		XPAR_PMODHYGRO_0_AXI_LITE_TMR_BASEADDR,
-    		XPAR_PMODHYGRO_0_DEVICE_ID,
-    		TIMER_FREQ_HZ // the clock frequency of the AXI bus, used to convert timer data
-	);
-    xil_printf("Init Done\n\r");
+void DemoInitialize() {
+   EnableCaches();
+   xil_printf("Init Started\n\r");
+   HYGRO_begin(
+      &myDevice,
+      XPAR_PMODHYGRO_0_AXI_LITE_IIC_BASEADDR,
+      0x40, // Chip address of PmodHYGRO IIC
+      XPAR_PMODHYGRO_0_AXI_LITE_TMR_BASEADDR,
+      XPAR_PMODHYGRO_0_DEVICE_ID,
+      TIMER_FREQ_HZ // Clock frequency of AXI bus, used to convert timer data
+   );
+   xil_printf("Init Done\n\r");
 }
 
-void DemoCleanup()
-{
-	DisableCaches();
+void DemoCleanup() {
+   DisableCaches();
 }
 
-void DemoRun()
-{
-	float temp_degc, hum_perrh, temp_degf;
-    while (1) {
-		temp_degc = HYGRO_getTemperature(&myDevice);
-		temp_degf = HYGRO_tempC2F(temp_degc);
-		hum_perrh = HYGRO_getHumidity(&myDevice);
-		xil_printf("Temperature: %d.%d deg F  Humidity: %d.%d RH\n\r",
-			(int)temp_degf, ((int)(temp_degf*100))%100,
-			(int)hum_perrh, ((int)(hum_perrh*100))%100
-		); // %f does not work with xil_printf, instead converting float to a pair of ints to display %.2f.
-		DemoSleep(1000); // 1 sample per second maximum, as per 9.2.1 in HDC1080 reference manual
-    }
+void DemoRun() {
+   float temp_degc, hum_perrh, temp_degf;
+   while (1) {
+      temp_degc = HYGRO_getTemperature(&myDevice);
+      temp_degf = HYGRO_tempC2F(temp_degc);
+      hum_perrh = HYGRO_getHumidity(&myDevice);
+      xil_printf(
+         "Temperature: %d.%d deg F  Humidity: %d.%d RH\n\r",
+         (int) temp_degf,
+         ((int) (temp_degf * 100)) % 100,
+         (int) hum_perrh,
+         ((int) (hum_perrh * 100)) % 100
+      );
+      // %f does not work with xil_printf
+      // instead, converting float to a pair of ints to display %.2f.
+
+      // 1 sample per second maximum, as per 9.2.1 in HDC1080 reference manual
+      DemoSleep(1000);
+   }
 }
 
-void DemoSleep(int millis)
-{
-#ifdef __MICROBLAZE__
-	MB_Sleep(millis);
-#else
-	usleep(1000 * millis);//delay for param microseconds
-#endif
+void DemoSleep(int millis) {
+	usleep(1000 * millis); // Delay for param microseconds
 }
 
-void EnableCaches()
-{
+void EnableCaches() {
 #ifdef __MICROBLAZE__
 #ifdef XPAR_MICROBLAZE_USE_ICACHE
-    Xil_ICacheEnable();
+   Xil_ICacheEnable();
 #endif
 #ifdef XPAR_MICROBLAZE_USE_DCACHE
-	Xil_DCacheEnable();
+   Xil_DCacheEnable();
 #endif
 #endif
 }
 
-void DisableCaches()
-{
+void DisableCaches() {
 #ifdef __MICROBLAZE__
 #ifdef XPAR_MICROBLAZE_USE_ICACHE
-	Xil_ICacheDisable();
+   Xil_ICacheDisable();
 #endif
 #ifdef XPAR_MICROBLAZE_USE_DCACHE
-	Xil_DCacheDisable();
+   Xil_DCacheDisable();
 #endif
 #endif
 }
--- ip/Pmods/PmodHYGRO_v1_0/drivers/PmodHYGRO_v1_0/src/PmodHYGRO.h
+++ ip/Pmods/PmodHYGRO_v1_0/drivers/PmodHYGRO_v1_0/src/PmodHYGRO.h
@@ -1,65 +1,45 @@
-/************************************************************************/
-/*																		*/
-/*	HYGROI2C.h		--		Declaration for HYGROI2C library		 	*/
-/*																		*/
-/************************************************************************/
-/*	Author:		Arthur Brown											*/
-/*	Copyright 2017, Digilent Inc.										*/
-/************************************************************************/
-/*  File Description:													*/
-/*	This file declares the HYGROI2C library functions and the constants	*/
-/*	involved.															*/
-/*																		*/
-/************************************************************************/
-/*  Revision History:													*/
-/*																		*/
-/*	01/30/2017(ArtVVB): created											*/
-/*	02/21/2017(ArtVVB): validated										*/
-/*																		*/
-/************************************************************************/
+/******************************************************************************/
+/*                                                                            */
+/* HYGROI2C.h -- Declaration for HYGROI2C library                             */
+/*                                                                            */
+/******************************************************************************/
+/* Author: Arthur Brown                                                       */
+/* Copyright 2017, Digilent Inc.                                              */
+/******************************************************************************/
+/* File Description:                                                          */
+/*                                                                            */
+/* This file declares the HYGROI2C library functions and the constants        */
+/* involved.                                                                  */
+/*                                                                            */
+/******************************************************************************/
+/* Revision History:                                                          */
+/*                                                                            */
+/*    01/30/2017(ArtVVB):   created                                           */
+/*    02/21/2017(ArtVVB):   validated                                         */
+/*    11/08/2017(atangzwj): Validated for Vivado 2016.4                       */
+/*                                                                            */
+/******************************************************************************/
 
 #ifndef PMODHYGRO_H
 #define PMODHYGRO_H
 
-/****************** Include Files ********************/
+/************ Include Files ************/
+
+#include "xiic.h"
+#include "xiic_l.h"
 #include "xil_types.h"
 #include "xstatus.h"
-#include "xiic_l.h"
-#include "xiic.h"
 #include "xtmrctr.h"
 
-/* ------------------------------------------------------------ */
-/*					Definitions									*/
-/* ------------------------------------------------------------ */
-#define bool u8
-#define true 1
-#define false 0
 
-#ifdef XPAR_XINTC_NUM_INSTANCES
- #include "xintc.h"
- #define INTC		XIntc
- #define INTC_HANDLER	XIntc_InterruptHandler
-#else
-#ifdef XPAR_SCUGIC_0_DEVICE_ID
- #include "xscugic.h"
- #define INTC		XScuGic
- #define INTC_HANDLER	XScuGic_InterruptHandler
-#else
-#define NO_IRPT 1
-#endif
-#endif
-/* ------------------------------------------------------------ */
-/*		Register addresses Definitions							*/
-/* ------------------------------------------------------------ */
+/************ Macro Definitions ************/
 
+// Register address definitions
 #define HYGRO_CONFIG 0x2
 #define HYGRO_HUM    0x1
 #define HYGRO_TEMP   0x0
 
-/* ------------------------------------------------------------ */
-/*				Bit masks Definitions							*/
-/* ------------------------------------------------------------ */
-
+// Bit mask definitions
 #define HYGRO_CONFIG_RST     0x8000
 #define HYGRO_CONFIG_HEAT    0x2000
 #define HYGRO_CONFIG_MODE    0x1000
@@ -70,34 +50,27 @@
 
 #define HYGRO_CONFIG_DEFAULT 0x1000
 
-/* ------------------------------------------------------------ */
-/*				Parameters Definitions							*/
-/* ------------------------------------------------------------ */
 
-/* ------------------------------------------------------------ */
-/*					Procedure Declarations						*/
-/* ------------------------------------------------------------ */
+/************ Type Definitions ************/
 
-typedef struct PmodHYGRO{
-	XIic HYGROIic;
-	XTmrCtr HYGROTimer;
-	u32 clockFreqHz;
-	u8 chipAddr;
-	u8 currentRegister;
-	u8 recvbytes;
-	u8* recv;
-#ifndef NO_IRPT
-	INTC intc;
-#endif
-}PmodHYGRO;
+typedef struct PmodHYGRO {
+   XIic HYGROIic;
+   XTmrCtr HYGROTimer;
+   u32 clockFreqHz;
+   u8 chipAddr;
+   u8 currentRegister;
+   u8 recvbytes;
+   u8* recv;
+} PmodHYGRO;
 
-void HYGRO_begin(PmodHYGRO* InstancePtr, u32 IIC_Address, u8 Chip_Address, u32 TMR_Address, UINTPTR TMR_DeviceId, u32 TMR_SysClockFreqHz);
+void HYGRO_begin(PmodHYGRO* InstancePtr, u32 IIC_Address, u8 Chip_Address,
+      u32 TMR_Address, UINTPTR TMR_DeviceId, u32 TMR_SysClockFreqHz);
 void HYGRO_end(PmodHYGRO* InstancePtr);
 void HYGRO_TimerInit(XTmrCtr* TMRInstancePtr, XTmrCtr_Config* TMRConfigPtr);
 int HYGRO_IICInit(XIic *IicInstancePtr);
-void HYGRO_ReadIIC(PmodHYGRO* InstancePtr, u8 reg, u8 *Data, int nData, u32 conversion_delay_ms);
+void HYGRO_ReadIIC(PmodHYGRO* InstancePtr, u8 reg, u8 *Data, int nData,
+      u32 conversion_delay_ms);
 void HYGRO_WriteIIC(PmodHYGRO* InstancePtr, u8 reg, u8 *Data, int nData);
-int HYGRO_SetupInterruptSystem(PmodHYGRO* InstancePtr, u32 interruptDeviceID, u32 interruptID, void* SendHandler,  void* ReceiveHandler);
 void HYGRO_DelayMillis(PmodHYGRO* InstancePtr, u32 millis);
 
 float HYGRO_getTemperature(PmodHYGRO *InstancePtr);

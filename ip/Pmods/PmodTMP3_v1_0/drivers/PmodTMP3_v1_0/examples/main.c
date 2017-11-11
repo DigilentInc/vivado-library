7a3eaf24e55f824c84618f4b5ace2633650c6d35
atangzwj <atangzwj@outlook.com>
2017-11-10 16:21:32 -0800

    ip: Pmods: PmodTMP3: Validated for 2016.4 Upgraded IPs Driver and example code changes    Removed printf, replaced with xil_printf    Code formatting Added .gitignore Added README

--- ip/Pmods/PmodTMP3_v1_0/drivers/PmodTMP3_v1_0/examples/main.c
+++ ip/Pmods/PmodTMP3_v1_0/drivers/PmodTMP3_v1_0/examples/main.c
@@ -1,106 +1,130 @@
-/*************************************************************************/
-/*                                                                       */
-/*     main.c --     PmodTMP3 Example Projects                           */
-/*                                                                       */
-/*************************************************************************/
-/*     Author: Arthur Brown                                              */
-/*     Copyright 2016, Digilent Inc.                                     */
-/*************************************************************************/
-/*  Module Description:                                                  */
-/*                                                                       */
-/*            This file contains code for running a demonstration of the */
-/*            PmodTMP3 when used with the PmodTMP3 IP core.              */
-/*                                                                       */
-/*************************************************************************/
-/*  Revision History:                                                    */
-/*                                                                       */
-/*            6/9/2016(ABrown): Created                                  */
-/* 			  5/8/2017(jPeyron): updated                                 */
-/*                                                                       */
-/*************************************************************************/
+/******************************************************************************/
+/*                                                                            */
+/* main.c -- PmodTMP3 example project                                         */
+/*                                                                            */
+/******************************************************************************/
+/* Author: Arthur Brown                                                       */
+/* Copyright 2016, Digilent Inc.                                              */
+/******************************************************************************/
+/* Module Description:                                                        */
+/*                                                                            */
+/* This file contains code for running a demonstration of the PmodTMP3 when   */
+/* used with the PmodTMP3 IP core.                                            */
+/*                                                                            */
+/******************************************************************************/
+/*  Revision History:                                                         */
+/*                                                                            */
+/*     06/09/2016(ABrown):   Created                                          */
+/*     05/08/2017(jPeyron):  updated                                          */
+/*     11/10/2017(atangzwj): Validated for Vivado 2016.4                      */
+/*                                                                            */
+/******************************************************************************/
 
-#include "xparameters.h"
-#include "xil_cache.h"
 #include "PmodTMP3.h"
+#include "sleep.h"
+#include "xil_cache.h"
 #include "xil_printf.h"
-#include <stdio.h>
+#include "xparameters.h"
 
 #ifdef __MICROBLAZE__
-    #include "microblaze_sleep.h"
-    //(deprecated in Vivado 2016.4)
+#define CPU_CLOCK_FREQ_MHZ (XPAR_CPU_CORE_CLOCK_FREQ_HZ / 1000000)
 #else
-    #include "sleep.h"
+#define CPU_CLOCK_FREQ_MHZ (XPAR_PS7_CORTEXA9_0_CPU_CLK_FREQ_HZ / 1000000)
 #endif
 
 void DemoInitialize();
 void DemoRun();
 void DemoCleanup();
+void EnableCaches();
+void DisableCaches();
 
 PmodTMP3 myDevice;
 
-#ifdef __MICROBLAZE__
-    #define CPU_CLOCK_FREQ_HZ (XPAR_CPU_CORE_CLOCK_FREQ_HZ / 1000000)
-#else
-    #define CPU_CLOCK_FREQ_HZ (XPAR_PS7_CORTEXA9_0_CPU_CLK_FREQ_HZ / 1000000)
-#endif
-
-
-
-
-int main(void)
-{
-	DemoInitialize();
-	DemoRun();
-	DemoCleanup();
-	return 0;
+int main(void) {
+   DemoInitialize();
+   DemoRun();
+   DemoCleanup();
+   return 0;
 }
 
-void DemoInitialize()
-{
+void DemoInitialize() {
+   EnableCaches();
+
+   xil_printf("\x1B[H");  // Move terminal cursor to top left
+   xil_printf("\x1B[1K"); // Clear terminal
+   xil_printf("Connected to PmodTMP3 Demo over UART\n\r");
+
+   TMP3_begin(&myDevice, XPAR_PMODTMP3_0_AXI_LITE_IIC_BASEADDR, TMP3_ADDR,
+         CPU_CLOCK_FREQ_MHZ);
+   xil_printf("Connected to PmodTMP3 over IIC on JB\n\r\n\r");
+}
+
+void DemoRun() {
+   double temp  = 0.0;
+   double temp2 = 0.0;
+   double temp3 = 0.0;
+
+   while (1) {
+      temp  = TMP3_getTemp(&myDevice);
+      temp2 = TMP3_CtoF(temp);
+      temp3 = TMP3_FtoC(temp2);
+
+      int temp2_round = 0;
+      int temp2_int   = 0;
+      int temp2_frac  = 0;
+      // Round to nearest hundredth, multiply by 100
+      if (temp2 < 0) {
+         temp2_round = (int) (temp2 * 1000 - 5) / 10;
+         temp2_frac  = -temp2_round % 100;
+      } else {
+         temp2_round = (int) (temp2 * 1000 + 5) / 10;
+         temp2_frac  = temp2_round % 100;
+      }
+      temp2_int = temp2_round / 100;
+
+      int temp3_round = 0;
+      int temp3_int   = 0;
+      int temp3_frac  = 0;
+      if (temp3 < 0) {
+         temp3_round = (int) (temp3 * 1000 - 5) / 10;
+         temp3_frac  = -temp3_round % 100;
+      } else {
+         temp3_round = (int) (temp3 * 1000 + 5) / 10;
+         temp3_frac  = temp3_round % 100;
+      }
+      temp3_int = temp3_round / 100;
+
+      xil_printf("Temperature: %d.%d in Fahrenheit\n\r", temp2_int, temp2_frac);
+      xil_printf("Temperature: %d.%d in Celsius\n\r", temp3_int, temp3_frac);
+      print("\n\r");
+      TMP3_delay(&myDevice, 1000000); // Delay
+   }
+}
+
+void DemoCleanup() {
+   TMP3_end(&myDevice);
+   xil_printf("PmodTMP3 Disconnected\n\r");
+   xil_printf("Closing UART Connection\n\r");
+}
+
+void EnableCaches() {
 #ifdef __MICROBLAZE__
 #ifdef XPAR_MICROBLAZE_USE_ICACHE
-    Xil_ICacheEnable();
+   Xil_ICacheEnable();
 #endif
 #ifdef XPAR_MICROBLAZE_USE_DCACHE
-    Xil_DCacheEnable();
+   Xil_DCacheEnable();
 #endif
 #endif
-	xil_printf("\x1B[H");//Move Terminal Cursor to top left
-	xil_printf("\x1B[1K");//Clear Terminal
-	xil_printf("Connected to PmodTMP3 Demo over UART\n\r");
-
-	TMP3_begin(&myDevice, XPAR_PMODTMP3_0_AXI_LITE_IIC_BASEADDR, TMP3_ADDR, CPU_CLOCK_FREQ_HZ );
-	xil_printf("Connected to PmodTMP3 over IIC on JB\n\r\n\r");
 }
 
-void DemoRun()
-{
-	double temp = 0.0;
-	double temp2 = 0.0;
-	double temp3 = 0.0;
-
-    while(1) {
-    	temp = TMP3_getTemp(&myDevice);
-    	temp2 = TMP3_CtoF(temp);
-        temp3 = TMP3_FtoC(temp2);
-       	printf("Temperature:  %.02lf  in Fahrenheit\n\r", temp2);//print double to 2 decimal places
-    	printf("Temperature:  %.02lf  in Celcius\n\r\n\r", temp3 );//print double to 2 decimal places
-    	TMP3_delay(&myDevice, 1000000);//delay
-    }
-}
-
-void DemoCleanup()
-{
+void DisableCaches() {
 #ifdef __MICROBLAZE__
-#ifdef XPAR_MICROBLAZE_USE_ICACHE
-    Xil_ICacheDisable();
-#endif
 #ifdef XPAR_MICROBLAZE_USE_DCACHE
-    Xil_DCacheDisable();
+   Xil_DCacheDisable();
+#endif
+#ifdef XPAR_MICROBLAZE_USE_ICACHE
+   Xil_ICacheDisable();
 #endif
 #endif
-	TMP3_end(&myDevice);
-	xil_printf("PmodTMP3 Disconnected\n\r");
-	xil_printf("Closing UART Connection\n\r");
-
 }
--- ip/Pmods/PmodTMP3_v1_0/drivers/PmodTMP3_v1_0/src/PmodTMP3.c
+++ ip/Pmods/PmodTMP3_v1_0/drivers/PmodTMP3_v1_0/src/PmodTMP3.c
@@ -1,343 +1,335 @@
-/*************************************************************************/
-/*                                                                       */
-/*     PmodTMP3.c --     PmodTMP3 Library                                */
-/*                                                                       */
-/*************************************************************************/
-/*     Author: Arthur Brown                                              */
-/*     Copyright 2016, Digilent Inc.                                     */
-/*************************************************************************/
-/*  Module Description:                                                  */
-/*                                                                       */
-/*            This file contains code for running a demonstration of the */
-/*            PmodTMP3 when used with the PmodTMP3 IP core.              */
-/*                                                                       */
-/*************************************************************************/
-/*  Revision History:                                                    */
-/*                                                                       */
-/*            6/9/2016(ABrown): Created                                  */
-/* 			  5/8/2017(jPeyron): updated                                 */
-/*                                                                       */
-/*************************************************************************/
-
+/******************************************************************************/
+/*                                                                            */
+/* PmodTMP3.c -- PmodTMP3 Library                                             */
+/*                                                                            */
+/******************************************************************************/
+/* Author: Arthur Brown                                                       */
+/* Copyright 2016, Digilent Inc.                                              */
+/******************************************************************************/
+/* Module Description:                                                        */
+/*                                                                            */
+/* This file contains code for running a demonstration of the PmodTMP3 when   */
+/* used with the PmodTMP3 IP core.                                            */
+/*                                                                            */
+/******************************************************************************/
+/* Revision History:                                                          */
+/*                                                                            */
+/*    06/09/2016(ABrown):   Created                                           */
+/*    05/08/2017(jPeyron):  updated                                           */
+/*    11/10/2017(atangzwj): Validated for Vivado 2016.4                       */
+/*                                                                            */
+/******************************************************************************/
 
 /***************************** Include Files *******************************/
+
 #include "PmodTMP3.h"
 
-
 /************************** Function Definitions ***************************/
+
 XIic_Config TMP3_Config =
 {
-
-	0,
-	0,
-	0,
-	2
-
+   0,
+   0,
+   0,
+   2
 };
 
 /* ------------------------------------------------------------ */
-/***	void TMP3_begin(PmodTMP3* InstancePtr, u32 IIC_Address)
+/*** void TMP3_begin(PmodTMP3* InstancePtr, u32 IIC_Address, u8 Chip_Address,
+**         u32 CpuClkFreqMHz)
 **
-**	Parameters:
-**		InstancePtr: A PmodTMP3 object to start
-**		IIC_Address: The Base address of the PmodTMP3 IIC
-**		Chip_Address: 7 bit Pmod device address
+**   Parameters:
+**      InstancePtr:  A PmodTMP3 object to start
+**      IIC_Address:  The Base address of the PmodTMP3 IIC
+**      Chip_Address: 7 bit Pmod device address
 **
-**	Return Value:
-**		none
+**   Return Value:
+**      none
 **
-**	Errors:
-**		none
+**   Errors:
+**      none
 **
-**	Description:
-**		Initialize the PmodTMP3.
+**   Description:
+**      Initialize the PmodTMP3.
 */
-void TMP3_begin(PmodTMP3* InstancePtr, u32 IIC_Address, u8 Chip_Address, u32 CpuClkFreqHz)
-{
-	TMP3_Config.BaseAddress=IIC_Address;
-	InstancePtr->ItersPerUSec = CpuClkFreqHz;
-	InstancePtr->chipAddr=Chip_Address;
-	TMP3_IICInit(&InstancePtr->TMP3Iic);
-	XIic_SetAddress(&InstancePtr->TMP3Iic, XII_ADDR_TO_SEND_TYPE, InstancePtr->chipAddr);
+void TMP3_begin(PmodTMP3* InstancePtr, u32 IIC_Address, u8 Chip_Address,
+      u32 CpuClkFreqMHz) {
+   TMP3_Config.BaseAddress = IIC_Address;
+   InstancePtr->ItersPerUSec = CpuClkFreqMHz;
+   InstancePtr->chipAddr = Chip_Address;
+   TMP3_IICInit(&InstancePtr->TMP3Iic);
+   XIic_SetAddress(&InstancePtr->TMP3Iic, XII_ADDR_TO_SEND_TYPE,
+         InstancePtr->chipAddr);
 }
 
 /* ------------------------------------------------------------ */
-/***	TMP3_end(void)
+/*** void TMP3_end(PmodTMP3* InstancePtr)
 **
-**	Parameters:
-**		InstancePtr		- PmodTMP3 object to stop
+**   Parameters:
+**      InstancePtr: PmodTMP3 object to stop
 **
-**	Return Value:
-**		none
+**   Return Value:
+**      none
 **
-**	Errors:
-**		none
+**   Errors:
+**      none
 **
-**	Description:
-**		Stops the device
+**   Description:
+**      Stops the device
 */
-void TMP3_end(PmodTMP3* InstancePtr){
-	XIic_Stop(&InstancePtr->TMP3Iic);
+void TMP3_end(PmodTMP3* InstancePtr) {
+   XIic_Stop(&InstancePtr->TMP3Iic);
 }
 
 /* ------------------------------------------------------------ */
-/***	TMP3_IICInit
+/*** int TMP3_IICInit(XIic *IicInstancePtr)
 **
-**	Parameters:
+**   Parameters:
+**      none
+**
+**   Return Value:
+**      none
+**
+**   Errors:
 **		none
 **
-**	Return Value:
-**		none
-**
-**	Errors:
-**		none
-**
-**	Description:
-**		Initializes the PmodTMP3 IIC.
-
+**   Description:
+**      Initializes the PmodTMP3 IIC.
 */
 
-int TMP3_IICInit(XIic *IicInstancePtr){
-	int Status;
+int TMP3_IICInit(XIic* IicInstancePtr) {
+   int Status;
 
-	Status = XIic_CfgInitialize(IicInstancePtr, &TMP3_Config, TMP3_Config.BaseAddress);
-	if (Status != XST_SUCCESS) {
-		return XST_FAILURE;
-	}
+   Status = XIic_CfgInitialize(IicInstancePtr, &TMP3_Config,
+         TMP3_Config.BaseAddress);
+   if (Status != XST_SUCCESS) {
+      return XST_FAILURE;
+   }
 
+   /*
+    * Start the IIC driver so that the device is enabled.
+    */
+   XIic_Start(IicInstancePtr);
 
+   /*
+    * Disable Global interrupt to use polled mode operation
+    */
+   XIic_IntrGlobalDisable(IicInstancePtr);
 
-	/*
-	 * Start the IIC driver so that the device is enabled.
-	 */
-	XIic_Start(IicInstancePtr);
-
-	/*
-	 * Disable Global interrupt to use polled mode operation
-	 */
-	XIic_IntrGlobalDisable(IicInstancePtr);
-
-	return XST_SUCCESS;
-
+   return XST_SUCCESS;
 }
 
 /* ------------------------------------------------------------ */
-/***	TMP3_ReadIIC
+/*** void TMP3_ReadIIC(PmodTMP3* InstancePtr, u8 reg, u8 *Data, int nData)
 **
-**	Parameters:
-**		InstancePtr - PmodTMP3 object to initialize
-**		reg			- Register to read from
-**		Data		- Pointer to recieve buffer
-**		nData		- Number of data values to read
+**   Parameters:
+**      InstancePtr: PmodTMP3 object to initialize
+**      reg:         Register to read from
+**      Data:        Pointer to recieve buffer
+**      nData:       Number of data values to read
 **
-**	Return Value:
-**		none
+**   Return Value:
+**      none
 **
-**	Errors:
-**		none
+**   Errors:
+**      none
 **
-**	Description:
-**		Reads nData data bytes from register reg
-**
+**   Description:
+**      Reads nData data bytes from register reg
 */
-void TMP3_ReadIIC(PmodTMP3* InstancePtr, u8 reg, u8 *Data, int nData)
-{
-	int Status;
-	Status = XIic_Start(&InstancePtr->TMP3Iic);
-	if (Status != XST_SUCCESS) {
-		return;
-	}
-	if (InstancePtr->currentRegister!=reg){
+void TMP3_ReadIIC(PmodTMP3* InstancePtr, u8 reg, u8 *Data, int nData) {
+   int Status;
+   Status = XIic_Start(&InstancePtr->TMP3Iic);
+   if (Status != XST_SUCCESS) {
+      return;
+   }
+   if (InstancePtr->currentRegister != reg) {
 
-		XIic_Send(InstancePtr->TMP3Iic.BaseAddress, InstancePtr->chipAddr, &reg, 1, XII_REPEATED_START_OPTION);
-		InstancePtr->currentRegister=reg;
-	}
-	XIic_Recv(InstancePtr->TMP3Iic.BaseAddress, InstancePtr->chipAddr, Data, nData, XIIC_STOP);
+      XIic_Send(InstancePtr->TMP3Iic.BaseAddress, InstancePtr->chipAddr, &reg,
+            1, XII_REPEATED_START_OPTION);
+      InstancePtr->currentRegister = reg;
+   }
+   XIic_Recv(InstancePtr->TMP3Iic.BaseAddress, InstancePtr->chipAddr, Data,
+         nData, XIIC_STOP);
 
-
-	Status = XIic_Stop(&InstancePtr->TMP3Iic);
-	if (Status != XST_SUCCESS) {
-		return;
-	}
+   Status = XIic_Stop(&InstancePtr->TMP3Iic);
+   if (Status != XST_SUCCESS) {
+      return;
+   }
 }
 
 /* ------------------------------------------------------------ */
-/***	TMP3_WriteIIC
+/*** void TMP3_WriteIIC(PmodTMP3* InstancePtr, u8 reg, u8* Data, int nData)
 **
-**	Parameters:
-**		InstancePtr - PmodTMP3 object to initialize
-**		reg			- Register to send to
-**		Data		- Pointer to data buffer to send
-**		nData		- Number of data values to send
+**   Parameters:
+**      InstancePtr: PmodTMP3 object to initialize
+**      reg:         Register to send to
+**      Data:        Pointer to data buffer to send
+**      nData:       Number of data values to send
 **
-**	Return Value:
-**		none
+**   Return Value:
+**      none
 **
-**	Errors:
-**		none
+**   Errors:
+**      none
 **
-**	Description:
-**		Writes nData data bytes to register reg
-**
+**   Description:
+**      Writes nData data bytes to register reg
 */
-void TMP3_WriteIIC(PmodTMP3* InstancePtr, u8 reg, u8 *Data, int nData)
-{
-	u8 out[10];
-	out[0]=reg;
-	out[1]=*Data;
-	int Status;
+void TMP3_WriteIIC(PmodTMP3* InstancePtr, u8 reg, u8* Data, int nData) {
+   u8 out[10];
+   out[0] = reg;
+   out[1] = *Data;
+   int Status;
 
-	if (InstancePtr->currentRegister!=reg){
-			InstancePtr->currentRegister=reg;
-	}
-	Status = XIic_Start(&InstancePtr->TMP3Iic);
-	if (Status != XST_SUCCESS) {
-		return;
-	}
-	XIic_Send(InstancePtr->TMP3Iic.BaseAddress, InstancePtr->chipAddr, out, nData+1, XIIC_STOP);
+   if (InstancePtr->currentRegister != reg) {
+      InstancePtr->currentRegister = reg;
+   }
+   Status = XIic_Start(&InstancePtr->TMP3Iic);
+   if (Status != XST_SUCCESS) {
+      return;
+   }
+   XIic_Send(InstancePtr->TMP3Iic.BaseAddress, InstancePtr->chipAddr, out,
+         nData + 1, XIIC_STOP);
 
-
-
-	Status = XIic_Stop(&InstancePtr->TMP3Iic);
-	if (Status != XST_SUCCESS) {
-		return;
-	}
-
-}
-
-
-
-/* ------------------------------------------------------------ */
-/***	void TMP3_config(PmodTMP3 *InstancePtr, u8 configuration)
-**
-**	Parameters:
-**		InstancePtr: A PmodTMP3 object to start
-**		configuration: 8 bit TMP3 configuration code, refer to "Configuration settings" under Bit Masks in header for details
-**
-**	Return Value:
-**		none
-**
-**	Errors:
-**		none
-**
-**	Description:
-**		Configures the PmodTMP3's Microchip TCN75A. Does not need to be called on startup.
-*/
-void TMP3_config(PmodTMP3 *InstancePtr, u8 configuration) {
-	u8 buf[1];
-	buf[0] = configuration;
-	TMP3_WriteIIC(InstancePtr, TMP3_REG_CONFIG, buf, 1);
+   Status = XIic_Stop(&InstancePtr->TMP3Iic);
+   if (Status != XST_SUCCESS) {
+      return;
+   }
 }
 
 /* ------------------------------------------------------------ */
-/***	double TMP3_getTemp(PmodTMP3 *InstancePtr)
+/*** void TMP3_config(PmodTMP3* InstancePtr, u8 configuration)
 **
-**	Parameters:
-**		InstancePtr: A PmodTMP3 object to start
+**   Parameters:
+**      InstancePtr:   A PmodTMP3 object to start
+**      configuration: 8 bit TMP3 configuration code, refer to "Configuration
+**                     settings" under Bit Masks in header for details
 **
-**	Return Value:
-**		tempResult: Current temperature seen by the TMP3 in degrees Celsius.
+**   Return Value:
+**      none
 **
-**	Errors:
-**		none
+**   Errors:
+**      none
 **
-**	Description:
-**		Requests and returns the temperature off of the Pmod TMP3
+**   Description:
+**      Configures the PmodTMP3's Microchip TCN75A. Does not need to be called on startup.
 */
-double TMP3_getTemp(PmodTMP3 *InstancePtr) {
-	double tempResult;
-	u16 uResult = 0;
-	int wResult = 0;
-	u8 buf[2] = {0};
-
-	//Read temperature register into buf.
-	TMP3_ReadIIC(InstancePtr, TMP3_REG_TEMP, buf, 2);
-
-	uResult = buf[1] | (buf[0] << 8);
-	if (buf[0] & 0x80 == 0) {
-		//result is positive
-		wResult = (int)uResult;
-	}
-	else {
-		//result negative -- two's complement
-		wResult = -1 * (int)(~uResult + 1);
-	}
-	
-	//9 bit resolution has 0.5 *C resolution, stored in the uppermost 9 bits of the buffer.
-	tempResult = (double)wResult / 256.0;
-
-	return tempResult;
+void TMP3_config(PmodTMP3* InstancePtr, u8 configuration) {
+   u8 buf[1];
+   buf[0] = configuration;
+   TMP3_WriteIIC(InstancePtr, TMP3_REG_CONFIG, buf, 1);
 }
 
 /* ------------------------------------------------------------ */
-/***	double TMP3_FtoC(double tempF)
+/*** double TMP3_getTemp(PmodTMP3* InstancePtr)
 **
-**	Parameters:
-**		tempF: a temperature value in degrees Fahrenheit
-**
-**	Return Value:
-**		tempC: a temperature value in degrees Celsius
-**
-**	Errors:
-**		none
-**
-**	Description:
-**		Converts temperature in Fahrenheit to Celsius
-*/
-double TMP3_FtoC(double tempF) {
-	double tempC;
-
-	tempC = (tempF - 32.0) / 1.8;
-
-	return tempC;
-}
-
-/* ------------------------------------------------------------ */
-/***	double TMP3_CtoF(double tempC)
-**
-**	Parameters:
-**		tempC: a temperature value in degrees Celsius
-**
-**	Return Value:
-**		tempF: a temperature value in degrees Fahrenheit
-**
-**	Errors:
-**		none
-**
-**	Description:
-**		Converts temperature in Celsius to Fahrenheit
-*/
-double TMP3_CtoF(double tempC) {
-	double tempF;
-
-	tempF = (tempC * 1.8) + 32.0;
-
-	return tempF;
-}
-
-/* ------------------------------------------------------------ */
-/***	void TMP3_delay(PmodTMP3* InstancePtr,int micros)
-**
-**	Parameters:
-**		micros: the number of microseconds to delay for
+**   Parameters:
 **      InstancePtr: A PmodTMP3 object to start
 **
-**	Return Value:
-**		none
+**   Return Value:
+**      tempResult: Current temperature seen by the TMP3 in degrees Celsius.
 **
-**	Errors:
-**		Slower than expected on Microblaze
+**   Errors:
+**      none
 **
-**	Description:
-**		Delays the processor for a number of microseconds
+**   Description:
+**      Requests and returns the temperature off of the Pmod TMP3
 */
-void TMP3_delay(PmodTMP3* InstancePtr, int micros ) {
-	int i;
+double TMP3_getTemp(PmodTMP3* InstancePtr) {
+   double tempResult;
+   u16 uResult = 0;
+   int wResult = 0;
+   u8 buf[2] = {0};
+
+   // Read temperature register into buf
+   TMP3_ReadIIC(InstancePtr, TMP3_REG_TEMP, buf, 2);
+
+   uResult = buf[1] | (buf[0] << 8);
+   if (buf[0] & 0x80 == 0) {
+      // Result is positive
+      wResult = (int) uResult;
+   } else {
+      // Result negative -- two's complement
+      wResult = -1 * (int) (~uResult + 1);
+   }
+
+   // 9 bit resolution has 0.5 *C resolution, stored in the uppermost 9 bits of
+   // the buffer
+   tempResult = (double) wResult / 256.0;
+
+   return tempResult;
+}
+
+/* ------------------------------------------------------------ */
+/*** double TMP3_FtoC(double tempF)
+**
+**   Parameters:
+**      tempF: a temperature value in degrees Fahrenheit
+**
+**   Return Value:
+**      tempC: a temperature value in degrees Celsius
+**
+**   Errors:
+**      none
+**
+**   Description:
+**      Converts temperature in Fahrenheit to Celsius
+*/
+double TMP3_FtoC(double tempF) {
+   double tempC;
+
+   tempC = (tempF - 32.0) / 1.8;
+
+   return tempC;
+}
+
+/* ------------------------------------------------------------ */
+/*** double TMP3_CtoF(double tempC)
+**
+**   Parameters:
+**      tempC: a temperature value in degrees Celsius
+**
+**   Return Value:
+**      tempF: a temperature value in degrees Fahrenheit
+**
+**   Errors:
+**      none
+**
+**   Description:
+**      Converts temperature in Celsius to Fahrenheit
+*/
+double TMP3_CtoF(double tempC) {
+   double tempF;
+
+   tempF = (tempC * 1.8) + 32.0;
+
+   return tempF;
+}
+
+/* ------------------------------------------------------------ */
+/*** void TMP3_delay(PmodTMP3* InstancePtr,int micros)
+**
+**   Parameters:
+**      micros: the number of microseconds to delay for
+**      InstancePtr: A PmodTMP3 object to start
+**
+**   Return Value:
+**      none
+**
+**   Errors:
+**      Slower than expected on Microblaze
+**
+**   Description:
+**      Delays the processor for a number of microseconds
+*/
+void TMP3_delay(PmodTMP3* InstancePtr, int micros) {
+   int i;
 #ifdef __MICROBLAZE__
-	for(i = 0; i < ((InstancePtr->ItersPerUSec*micros))/4; i++){
-		asm("");
-	}
+   for(i = 0; i < ((InstancePtr->ItersPerUSec*micros))/4; i++) {
+      asm("");
+   }
 #else
-	usleep(micros);
+   usleep(micros);
 #endif
 }

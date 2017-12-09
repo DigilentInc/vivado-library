f8694036c27d9431b6565ed77564ccd17b317649
atangzwj <atangzwj@outlook.com>
2017-12-09 14:12:10 -0800

    ip: Pmods: PmodMTDS: Began revising example code Added comment header to main.cc Removed calls to delay() with sleep() in MyDispDemo1.cc Formatted code in main.cc and MyDispDemo1.cc

--- ip/Pmods/PmodMTDS_v1_0/drivers/PmodMTDS_v1_0/examples/main.cc
+++ ip/Pmods/PmodMTDS_v1_0/drivers/PmodMTDS_v1_0/examples/main.cc
@@ -1,3 +1,23 @@
+/******************************************************************************/
+/*                                                                            */
+/* main.cc -- Main file for running PmodMTDS example programs                 */
+/*                                                                            */
+/******************************************************************************/
+/* Author: Thomas Kappenman                                                   */
+/* Copyright 2016, Digilent Inc. All rights reserved.                         */
+/******************************************************************************/
+/* Module Description:                                                        */
+/*                                                                            */
+/* Contains main function for running PmodMTDS example programs               */
+/*                                                                            */
+/******************************************************************************/
+/* Revision History:                                                          */
+/*                                                                            */
+/*    09/20/2016(tom21091): Created                                           */
+/*    12/09/2017(atangzwj): Validated for Vivado 2016.4                       */
+/*                                                                            */
+/******************************************************************************/
+
 #include "xparameters.h"
 #include "xil_cache.h"
 
@@ -7,38 +27,34 @@ extern void loop();
 void EnableCaches();
 void DisableCaches();
 
-int main ()
-{
-	EnableCaches();
-	setup();
-	while(1)
-	{
-		loop();
-	}
-	return 0;
+int main() {
+   EnableCaches();
+   setup();
+   while (1) {
+      loop();
+   }
+   DisableCaches();
+   return 0;
 }
 
-
-void EnableCaches()
-{
+void EnableCaches() {
 #ifdef __MICROBLAZE__
 #ifdef XPAR_MICROBLAZE_USE_ICACHE
-    Xil_ICacheEnable();
+   Xil_ICacheEnable();
 #endif
 #ifdef XPAR_MICROBLAZE_USE_DCACHE
-	Xil_DCacheEnable();
+   Xil_DCacheEnable();
 #endif
 #endif
 }
 
-void DisableCaches()
-{
+void DisableCaches() {
 #ifdef __MICROBLAZE__
 #ifdef XPAR_MICROBLAZE_USE_ICACHE
-	Xil_ICacheDisable();
+   Xil_ICacheDisable();
 #endif
 #ifdef XPAR_MICROBLAZE_USE_DCACHE
-	Xil_DCacheDisable();
+   Xil_DCacheDisable();
 #endif
 #endif
 }

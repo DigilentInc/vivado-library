a776d5e54ad757abac269d1b58d133aeb7e0d6c5
Arvin Tang <atangzwj@outlook.com>
2018-01-13 13:57:17 -0800

    ip: Pmods: PmodTMP3: Validated for Vivado 2017.4 Upgraded IP subcores Removed unused macro CPU_CLOCK_FREQ_MHZ in demo main.c Code formatting

--- ip/Pmods/PmodTMP3_v1_0/drivers/PmodTMP3_v1_0/src/PmodTMP3.h
+++ ip/Pmods/PmodTMP3_v1_0/drivers/PmodTMP3_v1_0/src/PmodTMP3.h
@@ -15,8 +15,9 @@
 /* Revision History:                                                          */
 /*                                                                            */
 /*    06/09/2016(ABrown):   Created                                           */
-/*    05/08/2017(jPeyron):  updated                                           */
+/*    05/08/2017(jPeyron):  Updated                                           */
 /*    11/10/2017(atangzwj): Validated for Vivado 2016.4                       */
+/*    01/13/2018(atangzwj): Validated for Vivado 2017.4                       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -40,21 +41,21 @@
 #define TMP3_REG_LIMITSET 0x3
 
 // Configuration settings
-#define TMP3_ONESHOT    0x80 //One Shot mode
-#define TMP3_RES9       0x00 //9-bit resolution
-#define TMP3_RES10      0x20 //10-bit resolution
-#define TMP3_RES11      0x40 //11-bit resolution
-#define TMP3_RES12      0x60 //12-bit resolution
-#define TMP3_FAULT1     0x00 //1 fault queue bits
-#define TMP3_FAULT2     0x08 //2 fault queue bits
-#define TMP3_FAULT4     0x10 //4 fault queue bits
-#define TMP3_FAULT6     0x18 //6 fault queue bits
-#define TMP3_ALERTLOW   0x00 //Alert bit active-low
-#define TMP3_ALERTHIGH  0x04 //Alert bit active-high
-#define TMP3_CMPMODE    0x00 //comparator mode
-#define TMP3_INTMODE    0x02 //interrupt mode
-#define TMP3_SHUTDOWN   0x01 //Shutdown enabled
-#define	TMP3_STARTUP    0x00 //Shutdown Disabled
+#define TMP3_ONESHOT   0x80 //One Shot mode
+#define TMP3_RES9      0x00 //9-bit resolution
+#define TMP3_RES10     0x20 //10-bit resolution
+#define TMP3_RES11     0x40 //11-bit resolution
+#define TMP3_RES12     0x60 //12-bit resolution
+#define TMP3_FAULT1    0x00 //1 fault queue bits
+#define TMP3_FAULT2    0x08 //2 fault queue bits
+#define TMP3_FAULT4    0x10 //4 fault queue bits
+#define TMP3_FAULT6    0x18 //6 fault queue bits
+#define TMP3_ALERTLOW  0x00 //Alert bit active-low
+#define TMP3_ALERTHIGH 0x04 //Alert bit active-high
+#define TMP3_CMPMODE   0x00 //comparator mode
+#define TMP3_INTMODE   0x02 //interrupt mode
+#define TMP3_SHUTDOWN  0x01 //Shutdown enabled
+#define	TMP3_STARTUP   0x00 //Shutdown Disabled
 
 //Device Constants
 #define TMP3_ADDR 0x48 // Based on jumpers JP1, JP2, and JP3
@@ -75,17 +76,17 @@ typedef struct PmodTMP3 {
    u8 chipAddr;
    u8 currentRegister;
    u8 recvbytes;
-   u8* recv;
+   u8 *recv;
 } PmodTMP3;
 
 
 /************ Function Prototypes ************/
 
-void TMP3_begin(PmodTMP3* InstancePtr, u32 IIC_Address, u8 Chip_Address);
-void TMP3_end(PmodTMP3* InstancePtr);
+void TMP3_begin(PmodTMP3 *InstancePtr, u32 IIC_Address, u8 Chip_Address);
+void TMP3_end(PmodTMP3 *InstancePtr);
 int  TMP3_IICInit(XIic *IicInstancePtr);
-void TMP3_ReadIIC(PmodTMP3* InstancePtr, u8 reg, u8 *Data, int nData);
-void TMP3_WriteIIC(PmodTMP3* InstancePtr, u8 reg, u8 *Data, int nData);
+void TMP3_ReadIIC(PmodTMP3 *InstancePtr, u8 reg, u8 *Data, int nData);
+void TMP3_WriteIIC(PmodTMP3 *InstancePtr, u8 reg, u8 *Data, int nData);
 void TMP3_config(PmodTMP3 *InstancePtr, u8 configuration);
 double TMP3_getTemp(PmodTMP3 *InstancePtr);
 double TMP3_FtoC(double tempF);

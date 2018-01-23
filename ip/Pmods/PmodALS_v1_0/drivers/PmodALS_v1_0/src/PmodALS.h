93495b50acbfd48c1016ddb15ae8c6cea89ded54
Arvin Tang <atangzwj@outlook.com>
2018-01-23 12:08:59 -0800

    ip: Pmods: PmodALS: Validated for Vivado 2017.4 Updated IP subcores Formatted drivers and example program

--- ip/Pmods/PmodALS_v1_0/drivers/PmodALS_v1_0/src/PmodALS.h
+++ ip/Pmods/PmodALS_v1_0/drivers/PmodALS_v1_0/src/PmodALS.h
@@ -1,19 +1,20 @@
-/************************************************************************/
-/*                                                                      */
-/* PmodALS.h    --      Driver definitions for the PmodALS              */
-/*                                                                      */
-/************************************************************************/
-/*  File Description:                                                   */
-/*                                                                      */
-/*  This file contains the drivers for the PmodALS IP from Digilent     */
-/*                                                                      */
-/************************************************************************/
-/*  Revision History:                                                   */
-/*                                                                      */
-/*  03/31/2016(tkappenman): Created                                     */
-/*  08/24/2017(artvvb): Validated for Vivado 2015.4                     */
-/*                                                                      */
-/************************************************************************/
+/******************************************************************************/
+/*                                                                            */
+/* PmodALS.h -- Driver definitions for the PmodALS                            */
+/*                                                                            */
+/******************************************************************************/
+/* File Description:                                                          */
+/*                                                                            */
+/* This file contains the drivers for the PmodALS IP from Digilent            */
+/*                                                                            */
+/******************************************************************************/
+/* Revision History:                                                          */
+/*                                                                            */
+/*    03/31/2016(tkappenman): Created                                         */
+/*    08/24/2017(artvvb):     Validated for Vivado 2015.4                     */
+/*    01/23/2018(atangzwj):   Validated for Vivado 2017.4                     */
+/*                                                                            */
+/******************************************************************************/
 
 #ifndef PMODALS_H
 #define PMODALS_H
@@ -21,24 +22,24 @@
 /****************** Include Files ********************/
 
 #include "xil_types.h"
-#include "xstatus.h"
-#include "xspi_l.h"
 #include "xspi.h"
+#include "xspi_l.h"
+#include "xstatus.h"
 
 /* ------------------------------------------------------------ */
 /*                  Definitions                                 */
 /* ------------------------------------------------------------ */
 
 typedef struct PmodALS {
-    XSpi ALSSpi;
+   XSpi ALSSpi;
 } PmodALS;
 
 /* ------------------------------------------------------------ */
 /*                  Procedure Declarations                      */
 /* ------------------------------------------------------------ */
 
-XStatus ALS_begin(PmodALS* InstancePtr, u32 SPI_Address);
+XStatus ALS_begin(PmodALS *InstancePtr, u32 SPI_Address);
 XStatus ALS_SPIInit(XSpi *SpiInstancePtr);
-u8 ALS_read(PmodALS* InstancePtr);
+u8 ALS_read(PmodALS *InstancePtr);
 
 #endif // PMODALS_H

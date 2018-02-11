e6fce00e8f138a478d94dfea17aa855907a9f20e
Arvin Tang <atangzwj@outlook.com>
2018-02-10 21:54:21 -0800

    ip: Pmods: PmodAD2: Validated for Vivado 2017.4 Updated IP subcores Formatted drivers and example program

--- ip/Pmods/PmodAD2_v1_0/drivers/PmodAD2_v1_0/examples/main.c
+++ ip/Pmods/PmodAD2_v1_0/drivers/PmodAD2_v1_0/examples/main.c
@@ -1,41 +1,39 @@
-/************************************************************************/
-/*                                                                      */
-/* main.c      demo project for the PmodAD2 IP                         */
-/*                                                                      */
-/************************************************************************/
-/*                                                                      */
-/*  Author:     Arthur Brown                                            */
-/*  Copyright 2016, Digilent Inc.                                       */
-/*                                                                      */
-/************************************************************************/
-/*  File Description:                                                   */
-/*                                                                      */
-/*  This demo reads conversion data captured from each of the four		*/
-/*  channels of the AD2. This data is printed to the terminal.          */
-/*                                                                      */
-/*  Data should be viewed with a serial terminal application          	*/
-/*  connected to your board and configured to use the appropriate baud  */
-/*  rate below.                                                         */
-/*                                                                      */
-/************************************************************************/
-/*  Revision History:                                                   */
-/*                                                                      */
-/*  06/13/2016(artvvb): Created                                         */
-/*  08/24/2017(artvvb): Validated for Vivado 2015.4                     */
-/*                                                                      */
-/************************************************************************/
-/*  Baud Rates:                                                         */
-/*                                                                      */
-/*  Microblaze: 9600 or other as specified in UARTlite core             */
-/*  Zynq: 115200                                                        */
-/*                                                                      */
-/************************************************************************/
+/******************************************************************************/
+/*                                                                            */
+/* main.c -- Demo project for the PmodAD2 IP                                  */
+/*                                                                            */
+/******************************************************************************/
+/* Author: Arthur Brown                                                       */
+/* Copyright 2016, Digilent Inc.                                              */
+/******************************************************************************/
+/* File Description:                                                          */
+/*                                                                            */
+/* This demo reads conversion data captured from each of the four channels of */
+/* the AD2. This data is printed to the terminal.                             */
+/*                                                                            */
+/* Data should be viewed with a serial terminal application connected to your */
+/* board and configured to use the appropriate Baud rate below.               */
+/*                                                                            */
+/******************************************************************************/
+/* Revision History:                                                          */
+/*                                                                            */
+/*    06/13/2016(artvvb):   Created                                           */
+/*    08/24/2017(artvvb):   Validated for Vivado 2015.4                       */
+/*    02/10/2018(atangzwj): Validated for Vivado 2017.4                       */
+/*                                                                            */
+/******************************************************************************/
+/* Baud Rates:                                                                */
+/*                                                                            */
+/*    MicroBlaze: 9600 or other as specified in UARTlite core                 */
+/*    Zynq: 115200                                                            */
+/*                                                                            */
+/******************************************************************************/
 
 #include <stdio.h>
-#include "xparameters.h"
-#include "xil_cache.h"
 #include "PmodAD2.h"
 #include "sleep.h"
+#include "xil_cache.h"
+#include "xparameters.h"
 
 void DemoInitialize();
 void DemoRun();
@@ -45,83 +43,74 @@ void DisableCaches();
 
 PmodAD2 myDevice;
 
-int main()
-{
-	DemoInitialize();
-	DemoRun();
-	DemoCleanup();
+int main() {
+   DemoInitialize();
+   DemoRun();
+   DemoCleanup();
 
-    return 0;
+   return 0;
 }
 
-
-
-void DemoInitialize()
-{
-	EnableCaches();
-	AD2_begin(&myDevice, XPAR_PMODAD2_0_AXI_LITE_IIC_BASEADDR, AD2_IIC_ADDR);
+void DemoInitialize() {
+   EnableCaches();
+   AD2_begin(&myDevice, XPAR_PMODAD2_0_AXI_LITE_IIC_BASEADDR, AD2_IIC_ADDR);
 }
 
+void DemoRun() {
+   u16 conv;
+   u8 channel;
+   double voltage;
 
-void DemoRun()
-{
-	u16 conv;
-	u8 channel;
-	double voltage;
+   xil_printf("Demo Started\n\r");
 
-	xil_printf("Demo Started\n\r");
+   // Turn on all channels
+   AD2_WriteConfig(&myDevice, AD2_DEFAULT_CONFIG);
 
-	//turn on all channels
-	AD2_WriteConfig(&myDevice, AD2_DEFAULT_CONFIG);
+   xil_printf("AD2 Configured\n\r");
 
-	xil_printf("AD2 Configured\n\r");
+   while (1) {
+      AD2_ReadConv(&myDevice, &conv);
 
-	while(1)
-	{
-		AD2_ReadConv(&myDevice, &conv);
+      // Scale captured data such that 0x000:0xFFF => 0.0:3.3
+      voltage = (double) (conv & AD2_DATA_MASK) * 3.3 / (AD2_DATA_MASK + 1);
 
-		//scale captured data such that 0x000:0xFFF => 0.0:3.3
-		voltage = (double)(conv & AD2_DATA_MASK) * 3.3 / (AD2_DATA_MASK + 1);
+      // Pull channel read information out of conv
+      channel = (conv & AD2_CHANNEL_MASK) >> AD2_CHANNEL_BIT;
 
-		//pull channel read information out of conv
-		channel = (conv & AD2_CHANNEL_MASK) >> AD2_CHANNEL_BIT;
+      printf("Pin V%d = %.02f V", channel + 1, voltage);
 
-		printf("Pin V%d = %.02f V", channel+1, voltage);
+      if (channel == 3) {
+         printf("\r\n");
+      } else {
+         printf("    ");
+      }
 
-		if (channel == 3)
-			printf("\r\n");
-		else
-			printf("    ");
-
-		usleep(10000);
-	}
+      usleep(10000);
+   }
 }
 
-void DemoCleanup()
-{
-	DisableCaches();
+void DemoCleanup() {
+   DisableCaches();
 }
 
-void EnableCaches()
-{
+void EnableCaches() {
 #ifdef __MICROBLAZE__
 #ifdef XPAR_MICROBLAZE_USE_ICACHE
-    Xil_ICacheEnable();
+   Xil_ICacheEnable();
 #endif
 #ifdef XPAR_MICROBLAZE_USE_DCACHE
-    Xil_DCacheEnable();
+   Xil_DCacheEnable();
 #endif
 #endif
 }
 
-void DisableCaches()
-{
+void DisableCaches() {
 #ifdef __MICROBLAZE__
 #ifdef XPAR_MICROBLAZE_USE_DCACHE
-    Xil_DCacheDisable();
+   Xil_DCacheDisable();
 #endif
 #ifdef XPAR_MICROBLAZE_USE_ICACHE
-    Xil_ICacheDisable();
+   Xil_ICacheDisable();
 #endif
 #endif
 }
--- ip/Pmods/PmodAD2_v1_0/drivers/PmodAD2_v1_0/src/PmodAD2.c
+++ ip/Pmods/PmodAD2_v1_0/drivers/PmodAD2_v1_0/src/PmodAD2.c
@@ -1,19 +1,20 @@
-/************************************************************************/
-/*																		*/
-/* PmodAD2.c	--		Driver source for the PmodAD2					*/
-/*																		*/
-/************************************************************************/
-/*  File Description:													*/
-/*																		*/
-/*	This file contains the drivers for the PmodAD2 IP from Digilent		*/
-/*																		*/
-/************************************************************************/
-/*  Revision History:													*/
-/*																		*/
-/*	06/13/2016(artvvb): Created											*/
-/*	08/21/2017(artvvb): Validated for Vivado 2015.4						*/
-/*																		*/
-/************************************************************************/
+/******************************************************************************/
+/*                                                                            */
+/* PmodAD2.c -- Driver source for the PmodAD2                                 */
+/*                                                                            */
+/******************************************************************************/
+/* File Description:                                                          */
+/*                                                                            */
+/* This file contains the drivers for the PmodAD2 IP from Digilent            */
+/*                                                                            */
+/******************************************************************************/
+/* Revision History:                                                          */
+/*                                                                            */
+/*    06/13/2016(artvvb):   Created                                           */
+/*    08/21/2017(artvvb):   Validated for Vivado 2015.4                       */
+/*    02/10/2018(atangzwj): Validated for Vivado 2017.4                       */
+/*                                                                            */
+/******************************************************************************/
 
 /***************************** Include Files *******************************/
 
@@ -23,125 +24,126 @@
 
 XIic_Config AD2_Config =
 {
-	0,
-	0,
-	0,
-	2
+   0,
+   0,
+   0,
+   2
 };
 
 /* ------------------------------------------------------------ */
-/***	void AD2_begin(PmodAD2* InstancePtr, u32 IIC_Address)
+/*** void AD2_begin(PmodAD2 *InstancePtr, u32 IIC_Address)
 **
-**	Parameters:
-**		InstancePtr: A PmodAD2 device to start
-**		IIC_Address: The base address of the PmodAD2 AXI IIC
-**		Chip_Address: 7 bit Pmod IIC device address
+**   Parameters:
+**      InstancePtr:  A PmodAD2 device to start
+**      IIC_Address:  The base address of the PmodAD2 AXI IIC
+**      Chip_Address: 7 bit Pmod IIC device address
 **
-**	Return Value:
-**		none
+**   Return Value:
+**      none
 **
-**	Description:
-**		Initializes the Pmod AD2 device
+**   Description:
+**      Initializes the PmodAD2 device
 */
-void AD2_begin(PmodAD2* InstancePtr, u32 IIC_Address, u8 Chip_Address)
-{
-	AD2_Config.BaseAddress=IIC_Address;
-	InstancePtr->chipAddr=Chip_Address;
-	AD2_IICInit(&InstancePtr->AD2Iic);
-	XIic_SetAddress(&InstancePtr->AD2Iic, XII_ADDR_TO_SEND_TYPE, InstancePtr->chipAddr);
+void AD2_begin(PmodAD2 *InstancePtr, u32 IIC_Address, u8 Chip_Address) {
+   AD2_Config.BaseAddress = IIC_Address;
+   InstancePtr->chipAddr = Chip_Address;
+   AD2_IICInit(&InstancePtr->AD2Iic);
+   XIic_SetAddress(&InstancePtr->AD2Iic, XII_ADDR_TO_SEND_TYPE,
+         InstancePtr->chipAddr);
 }
 
 /* ------------------------------------------------------------ */
-/***	AD2_IICInit
+/*** AD2_IICInit
 **
-**	Parameters:
-**		IicInstancePtr: Pmod AD2 AXI IIC device to initialize
+**   Parameters:
+**      IicInstancePtr: PmodAD2 AXI IIC device to initialize
 **
-**	Return Value:
-**		success or failure status
+**   Return Value:
+**      Success or failure status
 **
-**	Description:
-**		Initializes the PmodAD2 AXI IIC device
+**   Description:
+**      Initializes the PmodAD2 AXI IIC device
 */
-XStatus AD2_IICInit(XIic *IicInstancePtr){
-	int Status;
+XStatus AD2_IICInit(XIic *IicInstancePtr) {
+   int Status;
 
-	Status = XIic_CfgInitialize(IicInstancePtr, &AD2_Config, AD2_Config.BaseAddress);
-	if (Status != XST_SUCCESS) {
-		return Status;
-	}
+   Status = XIic_CfgInitialize(IicInstancePtr, &AD2_Config,
+         AD2_Config.BaseAddress);
+   if (Status != XST_SUCCESS) {
+      return Status;
+   }
 
-	//Start the IIC driver so that the device is enabled.
-	XIic_Start(IicInstancePtr);
+   // Start the IIC driver so that the device is enabled.
+   XIic_Start(IicInstancePtr);
 
-	//Disable Global interrupt to use polled mode operation
-	XIic_IntrGlobalDisable(IicInstancePtr);
+   // Disable Global interrupt to use polled mode operation
+   XIic_IntrGlobalDisable(IicInstancePtr);
 
-	return XST_SUCCESS;
+   return XST_SUCCESS;
 }
 
 /* ------------------------------------------------------------ */
-/***	XStatus AD2_ReadConv(PmodAD2* InstancePtr,	u16 *dataPtr)
+/*** XStatus AD2_ReadConv(PmodAD2 *InstancePtr, u16 *dataPtr)
 **
-**	Parameters:
-**		InstancePtr - PmodAD2 device to initialize
-**		Data		- Pointer to return data address
+**   Parameters:
+**      InstancePtr: PmodAD2 device to initialize
+**      dataPtr:     Pointer to return data address
 **
-**	Return Value:
-**		success or failure status
-**		conversion result and channel number at dataPtr
+**   Return Value:
+**      Success or failure status
+**      Conversion result and channel number at dataPtr
 **
-**	Description:
-**		Reads 2 data bytes from conversion register
+**   Description:
+**      Reads 2 data bytes from conversion register
 */
-XStatus AD2_ReadConv(PmodAD2* InstancePtr,	u16 *dataPtr)
-{
-	int Status;
-	u8 buf[2];
+XStatus AD2_ReadConv(PmodAD2 *InstancePtr, u16 *dataPtr) {
+   int Status;
+   u8 buf[2];
 
-	Status = XIic_Start(&InstancePtr->AD2Iic);
-	if (Status != XST_SUCCESS) {
-		return Status;
-	}
+   Status = XIic_Start(&InstancePtr->AD2Iic);
+   if (Status != XST_SUCCESS) {
+      return Status;
+   }
 
-	XIic_Recv(InstancePtr->AD2Iic.BaseAddress, InstancePtr->chipAddr, buf, 2, XIIC_STOP);
+   XIic_Recv(InstancePtr->AD2Iic.BaseAddress, InstancePtr->chipAddr, buf, 2,
+         XIIC_STOP);
 
-	*dataPtr = (buf[0] << 8) | buf[1];
+   *dataPtr = (buf[0] << 8) | buf[1];
 
-	Status = XIic_Stop(&InstancePtr->AD2Iic);
-	if (Status != XST_SUCCESS) {
-		return Status;
-	}
+   Status = XIic_Stop(&InstancePtr->AD2Iic);
+   if (Status != XST_SUCCESS) {
+      return Status;
+   }
 
-	return XST_SUCCESS;
+   return XST_SUCCESS;
 }
 
 /* ------------------------------------------------------------ */
-/***	XStatus AD2_WriteConfig(PmodAD2* InstancePtr, u8 configuration)
+/*** XStatus AD2_WriteConfig(PmodAD2 *InstancePtr, u8 configuration)
 **
-**	Parameters:
-**		InstancePtr   - PmodAD2 device to initialize
-**		configuration - value to load into the AD2 configuration register
-**						bit definitions are defined as AD2_CONFIG_* in PmodAD2.h
+**   Parameters:
+**      InstancePtr:   PmodAD2 device to initialize
+**      configuration: Value to load into the AD2 configuration register
+**                     Bit definitions are defined as AD2_CONFIG_* in PmodAD2.h
 **
-**	Return Value:
-**		success or failure status
+**   Return Value:
+**      Success or failure status
 **
-**	Description:
-**		Writes the configuration into the AD2 config register
+**   Description:
+**      Writes the configuration into the AD2 config register
 */
-XStatus AD2_WriteConfig(PmodAD2* InstancePtr, u8 configuration)
-{
-	int Status;
+XStatus AD2_WriteConfig(PmodAD2 *InstancePtr, u8 configuration) {
+   int Status;
 
-	Status = XIic_Start(&InstancePtr->AD2Iic);
-	if (Status != XST_SUCCESS) {
-		return Status;
-	}
+   Status = XIic_Start(&InstancePtr->AD2Iic);
+   if (Status != XST_SUCCESS) {
+      return Status;
+   }
 
-	XIic_Send(InstancePtr->AD2Iic.BaseAddress, InstancePtr->chipAddr, &configuration, 1, XIIC_STOP);
+   XIic_Send(InstancePtr->AD2Iic.BaseAddress, InstancePtr->chipAddr,
+         &configuration, 1, XIIC_STOP);
 
-	Status = XIic_Stop(&InstancePtr->AD2Iic);
+   Status = XIic_Stop(&InstancePtr->AD2Iic);
 
-	return Status;
+   return Status;
 }

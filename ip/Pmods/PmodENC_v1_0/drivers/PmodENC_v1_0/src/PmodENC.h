a29a362afb880bc708bc1a4b94ac21e52f1bb968
Arvin Tang <atangzwj@outlook.com>
2018-02-13 14:08:01 -0800

    ip: Pmods: PmodENC: Validated for Vivado 2017.4 Updated IP subcores Formatted drivers and example program Minor additions to README Added .gitignore

--- ip/Pmods/PmodENC_v1_0/drivers/PmodENC_v1_0/src/PmodENC.h
+++ ip/Pmods/PmodENC_v1_0/drivers/PmodENC_v1_0/src/PmodENC.h
@@ -1,33 +1,34 @@
-/************************************************************************/
-/*                                                                      */
-/* PmodENC.h	--		Header file for Pmod Encoder driver library		*/
-/*                                                                      */
-/************************************************************************/
-/*	Author:		Arthur Brown											*/
-/*     Copyright 2017, Digilent Inc.                                    */
-/************************************************************************/
-/*  Module Description:                                                 */
-/*                                                                      */
-/*      This file contains definitions for the driver and library for   */
-/*		the Pmod rotary shaft encoder.									*/
-/*                                                                      */
-/************************************************************************/
-/*  Revision History:                                                   */
-/*                                                                      */
-/*	04/19/2016(TommyK): Created 										*/
-/*	06/10/2016(ArtVVB): Adapted for PmodENC								*/
-/*  02/13/2017(ArtVVB): Validated for 2015.4                   			*/
-/*  10/25/2017(ArtVVB): Validated for 2016.4                   			*/
-/*                                                                      */
-/************************************************************************/
+/******************************************************************************/
+/*                                                                            */
+/* PmodENC.h -- Header file for Pmod Encoder driver library                   */
+/*                                                                            */
+/******************************************************************************/
+/* Author: Arthur Brown                                                       */
+/* Copyright 2017, Digilent Inc.                                              */
+/******************************************************************************/
+/* Module Description:                                                        */
+/*                                                                            */
+/* This file contains definitions for the driver and library for the Pmod     */
+/* rotary shaft encoder.                                                      */
+/*                                                                            */
+/******************************************************************************/
+/* Revision History:                                                          */
+/*                                                                            */
+/*    04/19/2016(TommyK):   Created                                           */
+/*    06/10/2016(ArtVVB):   Adapted for PmodENC                               */
+/*    02/13/2017(ArtVVB):   Validated for 2015.4                              */
+/*    10/25/2017(ArtVVB):   Validated for 2016.4                              */
+/*    02/13/2018(atangzwj): Validated for Vivado 2017.4                       */
+/*                                                                            */
+/******************************************************************************/
 
 #ifndef PmodENC_H
 #define PmodENC_H
 
 /****************** Include Files ********************/
 
-#include "xil_types.h"
 #include "xil_io.h"
+#include "xil_types.h"
 #include "xstatus.h"
 
 /******************* Definitions *********************/
@@ -37,13 +38,12 @@
 #define ENC_GPIO_PIN_BTN 0x4
 #define ENC_GPIO_PIN_SWT 0x8
 
-typedef struct PmodENC{
-	u32 GPIO_addr;
-}PmodENC;
+typedef struct PmodENC {
+   u32 GPIO_addr;
+} PmodENC;
 
-void ENC_begin(PmodENC* InstancePtr, u32 GPIO_Address);
-
-u32 ENC_getState(PmodENC* InstancePtr);
+void ENC_begin(PmodENC *InstancePtr, u32 GPIO_Address);
+u32 ENC_getState(PmodENC *InstancePtr);
 int ENC_getRotation(u32 state, u32 laststate);
 int ENC_switchOn(u32 state);
 int ENC_buttonPressed(u32 state);
